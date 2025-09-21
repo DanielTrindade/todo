9d5b75d2030212fcd9fc68b0432cbeaf55659bc9
Daniel Trindade <daniel.trindade@kodigos.com.br>
2025-09-21 01:09:41 -0400

    feat: adiciona suporte a CORS e atualiza variáveis de ambiente

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import { cors } from "@elysiajs/cors";
 import { jwt as jwtPlugin } from "@elysiajs/jwt";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
@@ -10,47 +11,50 @@ import { userRoutes } from "./routes/users";
 import { getUserId } from "./utils/get-user-id";
 
 export const app = new Elysia({
-	cookie: { secrets: [env.COOKIE_SECRET] },
+    cookie: { secrets: [env.COOKIE_SECRET] },
 })
-	.use(errorHandler)
-	.use(
-		jwtPlugin({
-			secret: env.JWT_SECRET,
-			name: "jwt",
-		}),
-	)
-	.use(
-		swagger({
-			documentation: {
-				info: {
-					title: "Todo App documentation",
-					version: "1.0.0",
-				},
-				tags: [
-					{ name: "Auth", description: "Authentication endpoints" },
+    .use(
+        cors({
+            origin: env.CORS_ORIGINS,
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            credentials: true,
+            allowedHeaders: ["Content-Type", "Authorization"],
+        }),
+    )
+    .use(errorHandler)
+    .use(
+        jwtPlugin({
+            secret: env.JWT_SECRET,
+            name: "jwt",
+        }),
+    )
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "Todo App documentation",
+                    version: "1.0.0",
+                },
+                tags: [
+                    { name: "Auth", description: "Authentication endpoints" },
                     { name: "Health", description: "Health check endpoint" },
                     { name: "Todos", description: "Todos endpoints" },
-                    { name: "Users", description: "Users endpoints" }
-				],
-			},
-			path: "/v1/swagger",
+                    { name: "Users", description: "Users endpoints" },
+                ],
+            },
+            path: "/v1/swagger",
             scalarConfig: {
                 spec: {
                     url: "/v1/swagger/json",
                 },
             },
-		}),
-	)
-	.decorate("userId", getUserId)
-	.onRequest(({ set }) => {
-		set.headers["Access-Control-Allow-Origin"] = "*";
-		set.headers["Access-Control-Allow-Methods"] =
-			"GET, POST, PUT, DELETE, OPTIONS";
-		set.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization";
-	})
-	.use(healthRoutes)
-	.use(authRoutes)
-	.use(todoRoutes)
-	.use(userRoutes);
+        }),
+    )
+    .decorate("userId", getUserId)
+    .use(healthRoutes)
+    .use(authRoutes)
+    .use(todoRoutes)
+    .use(userRoutes);
 
 export default app;
+
